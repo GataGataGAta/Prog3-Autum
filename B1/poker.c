1e2cf838277258ee553fdc64da17700016a84f9b
gata <hayato.yamagata@icloud.com>
2023-09-30 22:37:35 +0900

    Poker_push

--- B1/poker.c
+++ B1/poker.c
@@ -8,7 +8,6 @@ int main(void)
     int n[numbers];
     int c[cards];
     int i, j;
-    //int count1, count2;
     for (i = 0; i < cards; i++)
     {
         c[i] = 0;
@@ -17,27 +16,103 @@ int main(void)
     {
         n[i] = 0;
     }
-    
+
     i = 0;
     for (;;)
     {
         fprintf(stderr, "整数を入力してください:");
-        if (scanf("%d", &n[i]) == EOF)
+        for (i = 0; i < numbers; i++)
         {
-            break;
+            if (scanf("%d", &n[i]) == EOF)
+            {
+                return 0;
+            }
+            c[n[i]]++;
         }
-        c[n[i]]++;
-        i++;
-    }
-    printf("\n");
-    for(i = 0; i < cards; i++)
-    {
-        printf("%d %d\n", i, c[i]);
-    }
 
-    for (i = 0; i < cards; i++)
-    {
-        
+        // for(i = 0; i < cards; i++)
+        // {
+        //     printf("%d %d\n", i, c[i]);
+        // }
+        // 確認用に出力
+
+        int fc = 0; // 4カードを判定する変数
+        int fh = 0; // フルハウスを判定する変数
+        int tc = 0; // 3カードを判定する変数
+        int tp = 0; // 2ペアを判定する変数
+        int op = 0; // 1ペアを判定する変数
+
+        for (i = 0; i < cards; i++)
+        {
+            if (c[i] == 4)
+            {
+                fc = 1;
+                break;
+            }
+
+            else if (c[i] == 3)
+            {
+                tc = 1; // 3枚揃った段階
+                for (j = 0; j < cards; j++)
+                {
+                    if (i != j && c[j] == 2)
+                    {
+                        fh = 1; // フルハウスの判定
+                        break;
+                    }
+                }
+            }
+
+            else if (c[i] == 2)
+            {
+                op = 1; // 1ペアは確定
+                for (j = 0; j < cards; j++)
+                {
+                    if (j != i && c[j] == 2)
+                    {
+                        tp = 1; // 2ペアの判定
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (fc == 1)
+        {
+            printf("four of a kind\n");
+        }
+        else if (tc == 1 && fh == 0)
+        {
+            printf("three of a kind\n");
+        }
+
+        else if (fh == 1)
+        {
+            printf("full house\n");
+        }
+        else if (tp == 1)
+        {
+            printf("two pair\n");
+        }
+
+        else if (tp == 0 && op == 1)
+        {
+            printf("one pair\n");
+        }
+
+        else
+        {
+            printf("no pair\n");
+        }
+
+        for (i = 0; i < cards; i++)
+        {
+            c[i] = 0;
+        }
+        for (i = 0; i < numbers; i++)
+        {
+            n[i] = 0;
+        }
     }
 
     return 0;
--- B1/test.c
+++ B1/test.c
@@ -1,43 +1,63 @@
 #include <stdio.h>
 
 int main() {
-    int k, n, m;
-    printf("k : ");
-    scanf("%d", &k);
+    int n[5];
+    int c[14] = {0}; // Array to count the frequency of each number
+    int numbers = 5; // Number of integers to input
 
-    int a[10];
-    printf("%d個の整数 : ", k);
-    for (int i = 0; i < k; i++) {
-        scanf("%d", &a[i]);
-    }
-
-    printf("n : ");
-    scanf("%d", &n);
-
-    printf("m : ");
-    scanf("%d", &m);
-
-    for (int i = 0; i < m; i++) {
-        int b[16];
-        printf("%d個の整数: ", n);
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &b[j]);
+    for (;;) {
+        fprintf(stderr, "整数を入力してください:");
+        for (int i = 0; i < numbers; i++) {
+            if (scanf("%d", &n[i]) == EOF) {
+                return 0; // Exit program if EOF is reached
+            }
+            c[n[i]]++;
         }
 
-        int match = 1;
-        for (int j = 0; j <= n - k; j++) {
-            for (int l = 0; l < k; l++) {
-                if (b[j + l] != a[l]) {
-                    match = 0;
-                    break;
+        int hasFourOfAKind = 0;
+        int hasFullHouse = 0;
+        int hasThreeOfAKind = 0;
+        int hasTwoPair = 0;
+        int hasOnePair = 0;
+
+        for (int i = 1; i <= 13; i++) {
+            if (c[i] == 4) {
+                hasFourOfAKind = 1;
+            } else if (c[i] == 3) {
+                hasThreeOfAKind = 1;
+                for (int j = 1; j <= 13; j++) {
+                    if (j != i && c[j] >= 2) {
+                        hasFullHouse = 1;
+                        break;
+                    }
+                }
+            } else if (c[i] == 2) {
+                if (hasOnePair) {
+                    hasTwoPair = 1;
+                } else {
+                    hasOnePair = 1;
                 }
             }
-            if (match) {
-                break;
-            }
         }
 
-        printf("%d\n", match);
+        if (hasFourOfAKind) {
+            printf("four of a kind\n");
+        } else if (hasFullHouse) {
+            printf("full house\n");
+        } else if (hasThreeOfAKind) {
+            printf("three of a kind\n");
+        } else if (hasTwoPair) {
+            printf("two pair\n");
+        } else if (hasOnePair) {
+            printf("one pair\n");
+        } else {
+            printf("no pair\n");
+        }
+
+        // Reset counts for the next hand
+        for (int i = 1; i <= 13; i++) {
+            c[i] = 0;
+        }
     }
 
     return 0;
