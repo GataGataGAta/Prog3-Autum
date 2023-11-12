bfd5dc429d05e76e3981eb2e979eb5134f617547
gata <hayato.yamagata@icloud.com>
2023-11-12 11:51:24 +0900

    B6終わりました

--- B6/list4time.c
+++ B6/list4time.c
@@ -19,7 +19,7 @@ int main()
         list_insert_uniq(p, myrand());
     }
     clk_end = clock();
-    printf("一つ目の乱数の挿入 = %g [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC);
+    printf("一つ目の乱数の挿入 = %g [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC / n);
     myrand_init();
     clk_start = clock();
     for (i = 0; i < n; i++)
@@ -27,14 +27,14 @@ int main()
         list_find(p, myrand());
     }
     clk_end = clock();
-    printf("(1)と同じ乱数の検索 = %g [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC);
+    printf("(1)と同じ乱数の検索 = %g [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC / n);
     clk_start = clock();
     for (i = 0; i < n; i++)
     {
         list_find(p, myrand());
     }
     clk_end = clock();
-    printf("新しい乱数の検索 = %g [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC);
+    printf("新しい乱数の検索 = %g [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC / n);
 
     return 0;
 }
